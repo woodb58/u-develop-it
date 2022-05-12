d799fcc4b32f44d2910b73499e43d9ddf9d729ce
woodb58 <woodb58@gmail.com>
2022-05-12 18:35:21 -0500

    added a single ; that i spent an hour looking for. This commit is dedicated to you ;

--- db/schema.sql
+++ db/schema.sql
@@ -3,4 +3,10 @@ CREATE TABLE candidates (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   industry_connected BOOLEAN NOT NULL
-)
+);
+
+CREATE TABLE parties (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
